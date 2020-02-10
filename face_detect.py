16d88347cb8b17e306a8e3076e8e3cf6a820d064
Queen Frimpong <queenfrimpong@Queens-MacBook-Air.local>
2020-02-09 19:21:24 -0800

    Updated face dtection method and performed feature extraction

--- face_detect.py
+++ face_detect.py
@@ -6,51 +6,55 @@
 #   line and display each on the screen with red boxes overlaid on each human
 #   face.
 #
-#   The examples/faces folder contains some jpg images of people.  You can run
-#   this program on them and see the detections by executing the
+#   You can run this program on images and see the detections by executing the
 #   following command:
-#       ./face_detector.py ../examples/faces/*.jpg
-#
-#   This face detector is made using the now classic Histogram of Oriented
-#   Gradients (HOG) feature combined with a linear classifier, an image
-#   pyramid, and sliding window detection scheme.  This type of object detector
-#   is fairly general and capable of detecting many types of semi-rigid objects
-#   in addition to human faces.  Therefore, if you are interested in making
-#   your own object detectors then read the train_object_detector.py example
-#   program.  
-#
-#
-# COMPILING/INSTALLING THE DLIB PYTHON INTERFACE
-#   You can install dlib using the command:
-#       pip install dlib
-#
-#   Alternatively, if you want to compile dlib yourself then go into the dlib
-#   root folder and run:
-#       python setup.py install
-#
-#   Compiling dlib should work on any operating system so long as you have
-#   CMake installed.  On Ubuntu, this can be done easily by running the
-#   command:
-#       sudo apt-get install cmake
-#
-#   Also note that this example requires Numpy which can be installed
-#   via the command:
-#       pip install numpy
+#       python3 face_detect.py png2jpg/*.jpg
+#   Remember to run from project folder
+
 
 import sys
-
+import cv2
 import dlib
+from sklearn.svm import SVC
 
-detector = dlib.get_frontal_face_detector()
+detector = dlib.get_frontal_face_detector() #Face detector
 win = dlib.image_window()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat") #Landmark identifier. Set the filename to whatever you named the downloaded file
 
 for f in sys.argv[1:]:
     print("Processing file: {}".format(f))
     img = dlib.load_rgb_image(f)
-    # The 1 in the second argument indicates that we should upsample the image
-    # 1 time.  This will make everything bigger and allow us to detect more
-    # faces.
-    dets = detector(img, 1)
+    
+#Facial landmarking begins
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    clahe_image = clahe.apply(gray)
+    detections = detector(clahe_image, 1) #Detect the faces in the image
+    xlist = []
+    ylist = []
+    landmarks = []
+    for k,d in enumerate(detections): #For each detected face
+        shape = predictor(clahe_image, d) #Get coordinates
+        for i in range(1,68): #There are 68 landmark points on each face
+            xlist.append(float(shape.part(i).x))
+            ylist.append(float(shape.part(i).y))
+            cv2.circle(img, (shape.part(i).x, shape.part(i).y), 1, (0,0,255), thickness=2) #For each point, draw a red circle with thickness2 on the original frame
+        for x, y in zip(xlist, ylist): #Store all landmarks in one list in the format x1,y1,x2,y2,etc.
+            landmarks.append(x)
+            landmarks.append(y)
+            
+    #Printing the stored features that have been extracted
+    if len(detections) > 0:
+        print("Landmarks:", landmarks)
+    else: #If no faces are detected, return error message to other function to handle
+        print("Error")
+    
+    #cv2.imshow("Image", img) #Display the frame
+    if cv2.waitKey(1) & 0xFF == ord('q'): #Exit program when the user presses 'q'
+       break
+#Facial landmarking ends
+    
+    dets = detector(clahe_image, 1)
     print("Number of faces detected: {}".format(len(dets)))
     for i, d in enumerate(dets):
         print("Detection {}: Left: {} Top: {} Right: {} Bottom: {}".format(
@@ -61,16 +65,11 @@ for f in sys.argv[1:]:
     win.add_overlay(dets)
     dlib.hit_enter_to_continue()
 
+    if (len(sys.argv[1:]) > 0):
+        img = dlib.load_rgb_image(sys.argv[1])
+        dets, scores, idx = detector.run(img, 1, -1)
+        for i, d in enumerate(dets):
+            print("Detection {}, score: {}, face_type:{}".format(d, scores[i], idx[i]))
 
-# Finally, if you really want to you can ask the detector to tell you the score
-# for each detection.  The score is bigger for more confident detections.
-# The third argument to run is an optional adjustment to the detection threshold,
-# where a negative value will return more detections and a positive value fewer.
-# Also, the idx tells you which of the face sub-detectors matched.  This can be
-# used to broadly identify faces in different orientations.
-if (len(sys.argv[1:]) > 0):
-    img = dlib.load_rgb_image(sys.argv[1])
-    dets, scores, idx = detector.run(img, 1, -1)
-    for i, d in enumerate(dets):
-        print("Detection {}, score: {}, face_type:{}".format(
-            d, scores[i], idx[i]))
+
+
